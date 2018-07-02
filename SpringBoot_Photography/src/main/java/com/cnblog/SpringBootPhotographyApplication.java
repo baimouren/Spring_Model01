562d483b41b38d1752aaeb5a13369ba6520a32d7
18321749330@163.com <18321749330@163.com>
2018-07-02 15:11:39 +0800

    初始化:2018-7-2 15:11:33

---
+++ SpringBoot_Photography/src/main/java/com/cnblog/SpringBootPhotographyApplication.java
@@ -0,0 +1,12 @@
+package com.cnblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPhotographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPhotographyApplication.class, args);
+	}
+}
