f18100ff7b43df678d7140449d09d5159fd63619
18321749330@163.com <18321749330@163.com>
2018-07-31 19:04:58 +0800

    未完成 security 安全设置

---
+++ SpringBoot_Photography/src/main/java/com/cnblog/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.cnblog.config;
+
+
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+
+public class WebSecurityConfig implements WebSecurityConfigurer<HttpSecurityBuilder>
+{
+
+    @Override
+    public void init(HttpSecurityBuilder builder) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void configure(HttpSecurityBuilder builder) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+}
