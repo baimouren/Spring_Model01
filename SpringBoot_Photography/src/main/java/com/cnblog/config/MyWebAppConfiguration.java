d128876c705650d56b537dd4c399b294a35d0ca1
18321749330@163.com <18321749330@163.com>
2018-07-31 16:37:20 +0800

    修改包名为:cinfig

--- SpringBoot_Photography/src/main/java/com/cnblog/config/MyWebAppConfiguration.java
+++ SpringBoot_Photography/src/main/java/com/cnblog/config/MyWebAppConfiguration.java
@@ -1,4 +1,4 @@
-package com.cnblog.configurer;
+package com.cnblog.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
