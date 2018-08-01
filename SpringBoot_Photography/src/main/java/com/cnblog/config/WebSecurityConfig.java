415f2b4e80f5774c30dbfd04ea04b653ab0c8a66
18321749330@163.com <18321749330@163.com>
2018-08-01 11:27:02 +0800

    实现security

--- SpringBoot_Photography/src/main/java/com/cnblog/config/WebSecurityConfig.java
+++ SpringBoot_Photography/src/main/java/com/cnblog/config/WebSecurityConfig.java
@@ -1,22 +1,32 @@
 package com.cnblog.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-
-public class WebSecurityConfig implements WebSecurityConfigurer<HttpSecurityBuilder>
-{
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void init(HttpSecurityBuilder builder) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // TODO Auto-generated method stub
-
+        super.configure(auth);
     }
 
     @Override
-    public void configure(HttpSecurityBuilder builder) throws Exception {
-        // TODO Auto-generated method stub
-
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        //允许加载js,css,静态资源：  https://blog.csdn.net/wanderlustLee/article/details/80021468
+        .antMatchers("/js/**","/css/**","/images/*","/fonts/**","/**/*.png","/**/*.jpg").permitAll()
+        .anyRequest().authenticated() //4
+        .and()
+        .formLogin()
+            .loginPage("/login.html")
+            .failureUrl("/login.html?error")
+            .permitAll() //5
+        .and()
+        .logout().permitAll() ;
+        super.configure(http);
     }
-
 }
